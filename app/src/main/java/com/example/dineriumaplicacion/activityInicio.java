62f6b55d2d9c48c5ef014c19809ea90921f1b168
CristopherMAC <72010644@continental.edu.pe>
2023-04-23 23:53:19 -0500

    Diseño de Interfaz Inicio

---
+++ app/src/main/java/com/example/dineriumaplicacion/activityInicio.java
@@ -0,0 +1,42 @@
+package com.example.dineriumaplicacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class activityInicio extends AppCompatActivity {
+
+    ProgressBar prbIniProgreso;
+    TextView txtIniProgreso;
+    EditText edtIniAhorro, edtIniPresupuesto;
+    Spinner spnIniTiempo;
+    ImageButton imgBtnIniConfiguracion, imgBtnIniInversiones;
+    Button btnIniObjetivos, btnIniRegistrarObjetivo, btnIniRegistrarGastos, btnIniReporte;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+
+        prbIniProgreso = (ProgressBar) findViewById(R.id.prbIniProgreso);
+        txtIniProgreso = (TextView) findViewById(R.id.txtIniProgreso);
+        edtIniAhorro = (EditText) findViewById(R.id.edtIniAhorro);
+        edtIniPresupuesto = (EditText) findViewById(R.id.edtIniPresupuesto);
+        spnIniTiempo = (Spinner) findViewById(R.id.spnIniTiempo);
+        imgBtnIniConfiguracion = (ImageButton) findViewById(R.id.imgBtnIniConfiguracion);
+        imgBtnIniInversiones = (ImageButton) findViewById(R.id.imgBtnIniInversiones);
+        btnIniObjetivos = (Button) findViewById(R.id.btnIniObjetivos);
+        btnIniRegistrarObjetivo = (Button) findViewById(R.id.btnIniRegistrarObjetivo);
+        btnIniRegistrarGastos = (Button) findViewById(R.id.btnIniRegistrarGastos);
+        btnIniReporte = (Button) findViewById(R.id.btnIniReporte);
+    }
+}
