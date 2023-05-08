214e31b36185c0db199f9ec6e2bdddf2bf3cc604
renatogrg <72505451@continental.edu.pe>
2023-05-08 09:50:30 -0500

    login logica v1

--- app/src/main/java/com/example/dineriumaplicacion/activityLogin.java
+++ app/src/main/java/com/example/dineriumaplicacion/activityLogin.java
@@ -3,12 +3,24 @@ package com.example.dineriumaplicacion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.gms.common.SignInButton;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class activityLogin extends AppCompatActivity {
+
+    Button btnGoogle, btnFb;
+    FirebaseAuth miAuth;
+    GoogleSignInClient mGoogleSignInClient;
+    CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnFb = findViewById(R.id.btnLoginFb);
+        btnGoogle = findViewById(R.id.btnLoginGoogle);
     }
 }
